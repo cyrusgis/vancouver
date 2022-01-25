8faba22d35c5c8575abbea28fcfb471fe9311e3f
cyrusgis <98184452+cyrusgis@users.noreply.github.com>
2022-01-25 15:23:10 -0800

    Update script.js

--- dist/script.js
+++ dist/script.js
@@ -42,7 +42,7 @@ var mapOptions = {
   zoomControl: false,
   attributionControl: false,
   center: [49.249094796388015, -123.14231872558592],
-  zoom: 6,
+  zoom: 8,
   layers: [basemaps.OpenStreetMaps]
 };
 
